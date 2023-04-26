3125cedf28422f739893f9e47a2256770e982c15
Mathias <mathias.halmetschlager@gmx.at>
2023-04-26 10:43:24 +0200

    prisma db and nexauth setup

--- src/components/ui/Paragraph.tsx
+++ src/components/ui/Paragraph.tsx
@@ -1,37 +1,42 @@
-import {FC, forwardRef} from 'react'
-import {VariantProps, cva} from 'class-variance-authority'
-import { HTMLAttributes } from 'react';
-import { cn } from '@/lib/utils';
+import { FC, forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { HTMLAttributes } from "react";
+import { cn } from "@/lib/utils";
 
-const paragraphVariants = cva("max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center",
-    {
-        variants:{
-            size:{
-                default: 'text-base sm:text-lg',
-                sm: 'text-sm, sm:text-base',
-            }
-        },
-        defaultVariants: {
-            size:'default',
-        }
-    }
-)
+const paragraphVariants = cva(
+  "max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center",
+  {
+    variants: {
+      size: {
+        default: "text-base sm:text-lg",
+        sm: "text-sm, sm:text-base",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  }
+);
 
-interface ParagraphProps extends HTMLAttributes<HTMLParagraphElement>, VariantProps<typeof paragraphVariants> {
+interface ParagraphProps
+  extends HTMLAttributes<HTMLParagraphElement>,
+    VariantProps<typeof paragraphVariants> {}
 
-}
+const Paragraph: FC<ParagraphProps> = forwardRef<
+  HTMLParagraphElement,
+  ParagraphProps
+>(({ className, size, children, ...props }, ref) => {
+  return (
+    <p
+      ref={ref}
+      {...props}
+      className={cn(paragraphVariants({ size, className }))}
+    >
+      {children}
+    </p>
+  );
+});
 
-const Paragraph : FC<ParagraphProps> = forwardRef<HTMLParagraphElement, ParagraphProps>(({
-    className, size, children, ...props
-}, ref) => {
-        return ( 
-            <p ref={ref} {...props} className={cn(paragraphVariants({size,className}))} >
-                {children}
-            </p>
-         );
-    }
-)
+Paragraph.displayName = "Paragraph";
 
-Paragraph.displayName = 'Paragraph'
- 
 export default Paragraph;
